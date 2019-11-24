1c53a4fd957df550c5a13f9d353c8eb116db47ac
Anderson Laurentino <alaurentino.br@gmail.com>
2019-11-24 16:00:20 -0300

    Add the models Candidato and Bem

---
+++ models/Bem.py
@@ -0,0 +1,97 @@
+
+
+class Bem:
+    def __init__(self, cod_tipo="", desc_tipo="", desc_detalhada="", valor=0):
+        self.__cod_tipo = cod_tipo # Código do tipo de bem
+        self.__desc_tipo = desc_tipo # Descrição do tipo de bem
+        self.__desc_detalhada = desc_detalhada # Descrição detalhada do bem
+        self.__valor = valor # Valor do bem
+    
+    def __str__(self):
+        out = f"""
+        {self.__cod_tipo} -- {self.__desc_tipo} -- {self.__valor} Descrição: {self.__desc_detalhada[:80]}
+        """
+
+        return out
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __lt__(self, item):
+        if isinstance(item, Bem):
+            return ((self.__valor, self.__cod_tipo, self.__desc_detalhada) < (item.__valor, item.__cod_tipo, item.__desc_detalhada))
+        elif type(item) == int:
+            return self.__valor < item
+        else:
+            raise ValueError("Invalid type comparation!")
+    
+    def __le__(self, item):
+        if isinstance(item, Bem):
+            return ((self.__valor, self.__cod_tipo, self.__desc_detalhada) <= (item.__valor, item.__cod_tipo, item.__desc_detalhada))
+        elif type(item) == int:
+            return self.__valor <= item
+        else:
+            raise ValueError("Invalid type comparation!")
+    
+    def __gt__(self, item):
+        if isinstance(item, Bem):
+            return ((self.__valor, self.__cod_tipo, self.__desc_detalhada) > (item.__valor, item.__cod_tipo, item.__desc_detalhada))
+        elif type(item) == int:
+            return self.__valor > item
+        else:
+            raise ValueError("Invalid type comparation!")
+    
+    def __ge__(self, item):
+        if isinstance(item, Bem):
+            return ((self.__valor, self.__cod_tipo, self.__desc_detalhada) >= (item.__valor, item.__cod_tipo, item.__desc_detalhada))
+        elif type(item) == int:
+            return self.__valor >= item
+        else:
+            raise ValueError("Invalid type comparation!")
+    
+    def __eq__(self, item):
+        if isinstance(item, Bem):
+            return self.__valor == item.__valor and self.__desc_detalhada == item.__desc_detalhada
+        elif type(item) == int:
+            return self.__valor == item
+    
+    def __ne__(self, item):
+        if isinstance(item, Bem):
+            return self.__valor != item.__valor or self.__desc_detalhada != item.__desc_detalhada
+        else:
+            return self.__valor != item
+    
+    @property
+    def desc_tipo(self):
+        return self.__desc_tipo
+
+    @desc_tipo.setter
+    def desc_tipo(self, x):
+        self.__desc_tipo = x
+
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @valor.setter
+    def valor(self, x):
+        self.__valor = x
+
+
+    @property
+    def cod_tipo(self):
+        return self.__cod_tipo
+
+    @cod_tipo.setter
+    def cod_tipo(self, x):
+        self.__cod_tipo = x
+
+
+    @property
+    def desc_detalhada(self):
+        return self.__desc_detalhada
+
+    @desc_detalhada.setter
+    def desc_detalhada(self, x):
+        self.__desc_detalhada = x
